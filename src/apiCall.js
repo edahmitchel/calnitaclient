e460c954ebd6a54e1f115604088a761c4a5d4257
mitchel <edahmitchel@example.com>
2022-10-21 23:20:14 +0100

    updated api call route

--- src/apiCall.js
+++ src/apiCall.js
@@ -3,7 +3,10 @@ export function sendMail(email, notify) {
   const fetchData = async (email) => {
     console.log({ email });
     try {
-      const { data } = await axios.post("/waitlist/add", { email });
+      const { data } = await axios.post(
+        "https://courageous-yak-veil.cyclic.app/waitlist/add",
+        { email }
+      );
       console.log(data);
     } catch (error) {
       console.log(error);
