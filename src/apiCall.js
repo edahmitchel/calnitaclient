8129ac9ace9b42e5deb990c035fa745a7f34a51e
mitchel <edahmitchel@example.com>
2022-11-16 21:49:16 +0100

    new build check

--- src/apiCall.js
+++ src/apiCall.js
@@ -1,11 +1,19 @@
 import axios from "axios";
 export function sendMail(email, notify) {
   const fetchData = async (email) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        // Authorization: `Bearer ${user.token}`,
+      },
+    };
     console.log({ email });
     try {
       const { data } = await axios.post(
         "https://courageous-yak-veil.cyclic.app/waitlist/add",
-        { email }
+        JSON.stringify({ email }),
+        config
       );
       console.log(data);
     } catch (error) {
